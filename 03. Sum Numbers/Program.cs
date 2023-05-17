7ec86737705e55a04d456e5e47b432736feefab7
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-17 04:51:21 +0300

    Add files via upload

---
+++ 03. Sum Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (sum < n)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+
+                sum += currentNum;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
