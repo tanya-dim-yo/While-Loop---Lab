4e443eb8b1c87de9fe0710362dd63381c4f15f93
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-17 04:53:44 +0300

    Add files via upload

---
+++ 05. Account Balance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double balance = 0.0;
+
+            while (input != "NoMoreMoney")
+            {
+                double amount = double.Parse(input);
+
+                if (amount < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+
+                balance += amount;
+                Console.WriteLine($"Increase: {amount:F2}");
+                input = Console.ReadLine();
+
+            }
+
+            Console.WriteLine($"Total: {balance:F2}");
+        }
+    }
+}
