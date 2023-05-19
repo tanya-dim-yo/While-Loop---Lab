53716e425ec5fbef9dbc1d58a289bbbad24430fc
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-19 05:16:21 +0300

    Add files via upload

---
+++ 08. Graduation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int grades = 1;
+            double sum = 0;
+            int excluded = 0;
+
+            while (grades <= 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+
+                if (grade < 4)
+                {
+                    excluded++;
+
+                    if (excluded > 1)
+                    {
+                        break;
+                    }
+                    continue;
+                }
+
+                sum += grade;
+                grades++;
+            }
+
+            double average = sum / 12.0;
+
+            if (excluded <= 1)
+            {
+                Console.WriteLine($"{name} graduated. Average grade: {average:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"{name} has been excluded at {grades} grade");
+
+            }
+        }
+    }
+}
